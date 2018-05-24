91f321e299e9a78daa638e9a991908f1e3a82f05
Pedro Parra Ortega <parraortega.pedro@gmail.com>
2018-05-24 16:02:58 +0200

    added jenkins user and added to docker group

--- app/docker/build.sh
+++ app/docker/build.sh
@@ -4,5 +4,5 @@ set -e
 export REGISTRY=$1
 export VERSION=$2
 
-docker build -t example-app:scratch .
-docker build -t $REGISTRY/app-example:$VERSION -f ./Dockerfile.build ../
+docker build -t example-app:scratch ./app/docker
+docker build -t $REGISTRY/app-example:$VERSION -f ./app/docker/Dockerfile.build ./app
