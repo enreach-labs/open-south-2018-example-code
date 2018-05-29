69b207d7cea9105855cf29eb9ab33619f8d72f51
franciscoj <francisco.javieravendanorubio@voiceworks.com>
2018-05-29 17:27:19 +0200

    skip e2e from parallel

--- app/docker/build.sh
+++ app/docker/build.sh
@@ -5,4 +5,6 @@ export REGISTRY=$1
 export VERSION=$2
 
 docker build -t example-app:scratch ./app/docker
-docker build -t $REGISTRY/app-example:$VERSION -f ./app/docker/Dockerfile.build ./app
+docker build -t $REGISTRY/termica:$VERSION -f ./app/docker/Dockerfile.build ./app
+docker login --username=summalabs --password=topsecret
+docker push $REGISTRY/termica:$VERSION
