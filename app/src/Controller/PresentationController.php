205051b715bacb6abdb6d5182f2fba862f0d550e
Pedro Parra Ortega <parraortega.pedro@gmail.com>
2018-05-29 12:26:34 +0200

    adding vendors for example purpose

--- app/src/Controller/PresentationController.php
+++ app/src/Controller/PresentationController.php
@@ -30,7 +30,7 @@ class PresentationController extends Controller
 
         return $this->render("impress/presentation.html.twig", [
             'layout'       => "impress/layout.html.twig",
-            'slides' => 2,
+            'slides' => 6,
         ]);
     }
 }
